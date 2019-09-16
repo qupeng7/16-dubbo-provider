8df6e883cca2ef2d53b99e8e27c55f2ac393339d
qupeng <qupeng7@126.com>
2019-09-16 22:03:20 +0800

    commit

---
+++ src/main/java/com/qupeng/service/impl/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.qupeng.service.impl;
+
+import com.qupeng.model.User;
+import com.qupeng.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public String sayHello() {
+        return "Hello, dubbo";
+    }
+
+    @Override
+    public User getUserById(Integer id) {
+        User user = new User();
+        user.setId(id);
+        return user;
+    }
+}
